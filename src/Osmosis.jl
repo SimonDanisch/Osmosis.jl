35095def0f55c3caa8f5559d806fca7e8eecf6aa
Simon Danisch <sdanisch@gmail.com>
2015-06-11 20:49:13 +0200

    update - segfaulting

--- src/Osmosis.jl
+++ src/Osmosis.jl
@@ -1,5 +1,45 @@
-module Osmosis
+using GLVisualize, GLWindow, Reactive, GeometryTypes, GLAbstraction
 
-# package code goes here
+const RS    = GLVisualize.ROOT_SCREEN
 
-end # module
+function y_partition(area, percent)
+	amount = percent / 100.0
+	p = lift(area) do r
+		(Rectangle{Int}(r.x, r.y, 4000, round(Int, r.h*amount)), 
+			Rectangle{Int}(r.x, round(Int, r.h*amount), r.w, round(Int, r.h*(1-amount))))
+	end
+	return lift(first, p), lift(last, p)
+end
+function x_partition(area, percent)
+	amount = percent / 100.0
+	p = lift(area) do r
+		(Rectangle{Int}(r.x, r.y, round(Int, r.w*amount), r.h ), 
+			Rectangle{Int}(round(Int, r.w*amount), r.y, round(Int, r.w*(1-amount)), r.h))
+	end
+	return lift(first, p), lift(last, p)
+end
+
+main_area, write_area = y_partition(RS.area, 20.0)
+
+const main_screen 	= Screen(RS, area=main_area)
+const write_screen 	= Screen(RS, area=write_area)
+
+
+text_stream = lift(x->"Ima push this so hard\nDude yeah neva now rite?$x\n", fpswhen(RS.inputs[:open], 1.0))
+
+visualize(text_stream.value)
+
+lift(text_stream) do teeext
+	#area_handle	= Input(Rectangle{Int}(0,100*length(main_screen.children), main_screen.area.value.w, 100))
+	#area  		= lift(area_handle, main_screen.area, main_screen.inputs[:scroll_y]) do h_area, m_area, y
+#		Rectangle{Int}(h_area.x, h_area.y+round(Int, y), m_area.w, h_area.h)
+#	end
+#	new_screen  = Screen(main_screen, area=area)
+	#println(text)
+	robj = visualize(teeext)
+	push!(GLVisualize.ROOT_SCREEN.renderlist, robj)
+end
+
+
+
+renderloop()
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,18 @@
-using Osmosis
-using Base.Test
-
-# write your own tests here
-@test 1 == 1
+function visualize_def(grid, t, kw_args=Dict())
+    println("def: ", myid())
+end
+function teeest(_)
+	data = rand(10^6)
+	for i=1:10
+		sin(data+data)
+	end
+	visualize_def(rand(Float32, 10,10), 1)
+end
+println("start: ", myid())
+t = Timer(teeest)
+start_timer(t, 0, 1)
+for i=1:10
+	println("for: ", myid())
+	sleep(1)
+end
+stop_timer(t)
